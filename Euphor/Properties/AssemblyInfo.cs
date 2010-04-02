8a3da49332aeba4696533d5822902750268ae85b
Dennis Honeyman <arcticuno@gmail.com>
2010-04-02 10:26:35 -0400

    Initial commit.

---
+++ Euphor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Euphor")]
+[assembly: AssemblyProduct("Euphor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("RIT")]
+
+[assembly: AssemblyCopyright("Copyright © RIT 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("856fa08a-1a78-46d7-a0ea-37ccd02eada4")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
