2810691f083d6706f2142cd4b94d95768babfed4
unknown <GDDAdmin@.(none)>
2010-04-03 12:20:17 -0400

    added classes

---
+++ Euphor/IOverworldEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Euphor
+{
+    interface IOverworldEntity
+    {
+        void DrawForOverworld(SpriteBatch spriteBatch, Map map, int x, int y);
+        int XPos { get; }
+        int YPos { get; }
+    }
+
+
+}
---
+++ Euphor/NPE.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Euphor
+{
+    class NPE : IOverworldEntity
+    {
+        private Action interactFunc;
+        private Texture2D texture;
+        private int xPos, yPos;
+        private Game game;
+
+        public NPE(Game game)
+        {
+            this.game = game;
+        }
+
+        public int XPos
+        {
+            get { return xPos; }
+        }
+
+        public int YPos
+        {
+            get { return yPos; }
+        }
+
+        public void DrawForOverworld(SpriteBatch spriteBatch, Map map, int x, int y)
+        {
+            spriteBatch.Draw(texture,
+                new Vector2(x + xPos * map.TileSize, y + yPos * map.TileSize), Color.White);
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            xPos = x;
+            yPos = y;
+        }
+
+        public void SetTexture(string textureName)
+        {
+            texture = game.Content.Load<Texture2D>(textureName);
+        }
+
+        public void SetInteractFunc(Action func)
+        {
+            interactFunc = func;
+        }
+
+        public void Interact()
+        {
+            interactFunc();
+        }
+    }
+}
